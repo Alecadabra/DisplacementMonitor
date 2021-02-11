45c70160c9ba20e28c282051db56001d2e4de61d
Alec PC <alecadab@gmail.com>
2021-02-11 10:15:36 +0800

    Large battery optimisation in scheduled measurement

--- app/src/main/java/displacement/monitor/android/activity/ScheduledMeasurementActivity.kt
+++ app/src/main/java/displacement/monitor/android/activity/ScheduledMeasurementActivity.kt
@@ -34,7 +34,7 @@ class ScheduledMeasurementActivity : AppCompatActivity() {
     // Handles all the very specific ways you make android turn on/off the device
     private val deviceStateController by lazy { DeviceStateController(this) }
 
-    private val influxController = RemoteDBController()
+    private val remoteDBController = RemoteDBController()
 
     /** Flag for if a value for distance has been measured */
     private var measured: Boolean = false
@@ -42,16 +42,15 @@ class ScheduledMeasurementActivity : AppCompatActivity() {
     private var failedAttempts = 0
 
     private val cameraFrameCallback = CameraFrameCallback { image ->
-        val preview = image.clone()
-
         try {
-            val distance = this.calibratedImageProcessor.measure(image, preview)
-            image.release()
-            val unixTimestamp = System.currentTimeMillis() / 1000L
-            CoroutineScope(Dispatchers.Main).launch {
-                if (!this@ScheduledMeasurementActivity.measured) {
-                    val fails = this@ScheduledMeasurementActivity.failedAttempts
-                    val measurement = Measurement(unixTimestamp, distance, fails)
+            val distance = this.calibratedImageProcessor.measure(image)
+            if (!this.measured) {
+                val measurement = Measurement(
+                    time = System.currentTimeMillis() / 1000L,
+                    distance = distance,
+                    failedAttempts = this.failedAttempts
+                )
+                CoroutineScope(Dispatchers.Main).launch {
                     this@ScheduledMeasurementActivity.onDistanceMeasured(measurement)
                 }
             }
@@ -67,22 +66,12 @@ class ScheduledMeasurementActivity : AppCompatActivity() {
                         launch { activity.views.cameraView.flashOn() }
                     }
 
-                    // Set text readout
-                    val failuresText = activity.failedAttempts.let {
-                        if (it < 10) {
-                            ".".repeat(it / 2)
-                        } else {
-                            "\nFailed Attempts: $it"
-                        }
-                    }
-                    val text = "Looking for target$failuresText"
-                    activity.views.readout.text = text
                     activity.failedAttempts++
                 }
             }
         }
 
-        return@CameraFrameCallback preview
+        return@CameraFrameCallback image
     }
 
     // Android entry points ------------------------------------------------------------------------
@@ -93,9 +82,6 @@ class ScheduledMeasurementActivity : AppCompatActivity() {
         this.deviceStateController.start()
 
         setContentView(R.layout.activity_scheduled_measurement)
-        this.title = "Measuring Distance"
-
-        Log.d("ScheduledMeasurement", "Activity started")
 
         this.views.cameraView.start(this.settings, this.cameraFrameCallback)
     }
@@ -125,8 +111,8 @@ class ScheduledMeasurementActivity : AppCompatActivity() {
 
             // Send measurement to Influx
             CoroutineScope(Dispatchers.IO).launch {
-                this@ScheduledMeasurementActivity.influxController.writeMeasurement(measurement)
-                this@ScheduledMeasurementActivity.influxController.close()
+                this@ScheduledMeasurementActivity.remoteDBController.writeMeasurement(measurement)
+                this@ScheduledMeasurementActivity.remoteDBController.close()
             }
 
             this.deviceStateController.finish()
@@ -137,7 +123,6 @@ class ScheduledMeasurementActivity : AppCompatActivity() {
 
     private inner class Views(
         val cameraView: CustomCameraView = findViewById(R.id.scheduledMeasurementActivityCameraView),
-        val readout: TextView = findViewById(R.id.scheduledMeasurementActivityReadout)
     )
 
     companion object {
