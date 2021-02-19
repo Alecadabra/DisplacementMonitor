11a2f4532986066623b0565b78983cc2cc81b533
Alec PC <alecadab@gmail.com>
2021-02-19 16:01:04 +0800

    Documentation

--- app/src/main/java/displacement/monitor/setup/android/fragment/AbstractSetupPageFragment.kt
+++ app/src/main/java/displacement/monitor/setup/android/fragment/AbstractSetupPageFragment.kt
@@ -4,8 +4,16 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import displacement.monitor.setup.android.activity.SetupSlidePagerActivity
 
+/**
+ * An abstract implementation of a page of a view pager that has a custom title text for the
+ * activity title bar. Designed to be placed in the frame of a [SetupSlidePagerActivity].
+ * @param title The text to set as the activity's title
+ */
 abstract class AbstractSetupPageFragment(private val title: String) : Fragment() {
 
+    /**
+     * Convenience property to obtain the parent activity cast as a [SetupSlidePagerActivity].
+     */
     protected val pagerActivity: SetupSlidePagerActivity
         get() {
             val localActivity = this.activity
